c926012e7888e0d98f6cd05e0e090d614b82774e
ChalkHuman <isacisboss718@gmail.com>
2020-06-09 01:03:09 -0500

    Trait

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,5 +1,6 @@
 import Entity from "./Entity.js";
 import Velocity from "./traits/Velocity.js";
+import Jump from "./traits/Jump.js";
 import {loadPlayerSprite} from "./sprites.js";
 
 export function createMelon () {
@@ -8,10 +9,11 @@ export function createMelon () {
         const melon = new Entity ();
 
         melon.addTrait (new Velocity ());
+        melon.addTrait (new Jump ());
 
         melon.draw = function drawMelon (context) {
             sprite.draw ("idle", context, this.pos.x, this.pos.y);
         }
         return melon;
-    })
+    });
 }
