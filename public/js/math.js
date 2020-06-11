54342bbd6fc1411f832dc49c286cffaff1f3fb4d
ChalkHuman <isacisboss718@gmail.com>
2020-06-10 19:13:32 -0500

    Grid

--- public/js/math.js
+++ public/js/math.js
@@ -2,6 +2,13 @@ export class Matrix {
     constructor () {
         this.grid = [];
     }
+    forEach (callback) {
+        this.grid.forEach ((column, x) => {
+            column.forEach ((value, y) => {
+                callback (value, x, y);
+            });
+        });
+    }
     get (x, y) {
         const col = this.grid [x];
         if (col) {
